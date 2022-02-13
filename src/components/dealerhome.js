701e65cf39d28ce74ef77c48cb3fec2f67ccfaf6
Izhan <babuizhan1234@gmail.com>
2022-02-13 20:28:50 +0530

    fixed submit button

--- src/components/dealerhome.js
+++ src/components/dealerhome.js
@@ -76,7 +76,7 @@ const Dealerhome = ({ dealerInfo }) => {
           </div>
         </div>
         <input
-          className="my-10 group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="cursor-pointer my-10 group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           type="submit"
         />
       </form>
